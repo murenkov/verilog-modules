4c2029f77d6cd4ca74d7755ee2c456b945eee129
murenkov <yaroslav.murenkov@gmail.com>
2020-09-27 00:42:54 +0300

    Add 8bit registr

---
+++ reg_8bit.v
@@ -0,0 +1,16 @@
+module reg_8bit(
+    input      [7:0] data,
+    input            enable,
+    input            reset,
+    input            clock,
+
+    output reg [7:0] q
+);
+
+always @(posedge clock or posedge reset) begin
+    if (reset) q <= 0;
+    else if (enable) q <= data;
+end
+
+endmodule
+
