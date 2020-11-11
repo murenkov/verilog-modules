92ed1877681aa938350f60028b875d554bb5072c
murenkov <yaroslav.murenkov@gmail.com>
2020-11-11 21:26:42 +0300

    Fix Transmitter

--- Transmitter.v
+++ Transmitter.v
@@ -6,28 +6,34 @@ module Transmitter(
     input [3:0] data_in,
     input       start,
 
-    output reg busy,
+    output           busy,
     output reg [6:0] tx
 );
 
+    assign busy = ~start;
+
     always @(*)
         if (start) begin
-            busy <= 1;
             case (data_in)
-                4'd0: tx <= 7'b1000000;
-                4'd1: tx <= 7'b1111001;
-                4'd2: tx <= 7'b0100100;
-                4'd3: tx <= 7'b0110000;
-                4'd4: tx <= 7'b0011001;
-                4'd5: tx <= 7'b0010010;
-                4'd6: tx <= 7'b0000010;
-                4'd7: tx <= 7'b1111000;
-                4'd8: tx <= 7'b0000000;
-                4'd9: tx <= 7'b0100000;
+                4'h0: tx = 7'b1000000;
+                4'h1: tx = 7'b1111001;
+                4'h2: tx = 7'b0100100;
+                4'h3: tx = 7'b0110000;
+                4'h4: tx = 7'b0011001;
+                4'h5: tx = 7'b0010010;
+                4'h6: tx = 7'b0000010;
+                4'h7: tx = 7'b1111000;
+                4'h8: tx = 7'b0000000;
+                4'h9: tx = 7'b0100000;
+                4'hA: tx = 7'b0001000;
+                4'hB: tx = 7'b0000011;
+                4'hC: tx = 7'b1000110;
+                4'hD: tx = 7'b0100001;
+                4'hE: tx = 7'b0000110;
+                4'hF: tx = 7'b0001110;
 
-                default: tx <= 7'b1111111;
+                default: tx = 7'b1111111;
             endcase
-            busy <= 0;
         end
 
 endmodule
